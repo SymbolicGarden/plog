62d7a9630c5e0a8555db0799b0efdc3f582c77d4
Zhongying Qiao <cryptoque@users.noreply.github.com>
2025-12-19 01:39:43 -0800

    update package title

--- pack.pl
+++ pack.pl
@@ -3,7 +3,7 @@
 % See the LICENSE file for details or http://www.apache.org/licenses/LICENSE-2.0.
 
 name(plog).
-title('Dynamic Prolog blog engine with Markdown and RSS').
+title('Dynamic Prolog blog engine with Markdown parser and RSS').
 version('1.0.2').
 keywords([blog,symbolic,engine,server,web,markdown,parser,rss]).
 author('Zhongying Qiao', 'https://blauanarchy.org/').
